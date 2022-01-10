4add5ed7b2a02a36bb95ccb517a28db2f33949a3
msalazar <msalazar@fedora>
2022-01-09 23:19:24 -0500

    Update by Mario Salazar

---
+++ proyecto_mipymesEJB/ejbModule/proyect_mipymes/model/clientes/managers/ManagerClientes.java
@@ -0,0 +1,20 @@
+package proyect_mipymes.model.clientes.managers;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerClientes
+ */
+@Stateless
+@LocalBean
+public class ManagerClientes {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerClientes() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ proyecto_mipymesEJB/ejbModule/proyect_mipymes/model/core/utils/ModelUtil.java
@@ -0,0 +1,65 @@
+package proyect_mipymes.model.core.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 
+ * @author msalazar
+ *
+ */
+public class ModelUtil {
+	/**
+	 * Verifica si una cadena es igual a null o tiene longitud igual a cero.
+	 * 
+	 * @param cadena Cadena que va a verificarse.
+	 * @return
+	 */
+	public static boolean isEmpty(String cadena) {
+		if (cadena == null || cadena.length() == 0)
+			return true;
+		return false;
+	}
+
+	/**
+	 * Devuelve el valor del anio actual.
+	 * 
+	 * @return valor entero del anio actual.
+	 */
+	public static int getAnioActual() {
+		Date fechaActual = new Date();
+		SimpleDateFormat formato = new SimpleDateFormat("yyyy");
+		int anioActual = Integer.parseInt(formato.format(fechaActual));
+		return anioActual;
+	}
+
+	/**
+	 * Adiciona o resta dias a una fecha de tipo Date.
+	 * 
+	 * @param date la fecha original.
+	 * @param days el numero de dias a sumar o restar.
+	 * @return la fecha resultante.
+	 */
+	public static Date addDays(Date date, int days) {
+		GregorianCalendar cal = new GregorianCalendar();
+		cal.setTime(date);
+		cal.add(Calendar.DATE, days);
+
+		return cal.getTime();
+	}
+
+	/**
+	 * Calcula el numero de dias entre dos fechas.
+	 * 
+	 * @param d1 fecha inicial
+	 * @param d2 fecha final
+	 * @return Numero de dias.
+	 */
+	public static long getDifferenceDays(Date d1, Date d2) {
+		long diff = d2.getTime() - d1.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+}
